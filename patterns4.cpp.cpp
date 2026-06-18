66beb6cc2590007c250e91860b28b48b6d8f07b4
Code-Dawg <whatsupcodedwag@gmail.com>
2026-06-18 10:54:37 +0545

    Add pattern programs

---
+++ patterns4.cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=3;
+    int add_num=1;
+    for (int i=1;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            cout<<add_num;
+            add_num++;
+        }
+        cout<<endl;
+        
+        
+
+    }
+    return 0;
+}
---
+++ pattrens6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=4;
+
+    
+    for (int i=0;i<=4;i++){
+
+        for(int j=1;j<=i+1;j++){
+
+            cout<<i+1;
+        
+            
+        }
+        cout<<endl;
+
+    }
+}
